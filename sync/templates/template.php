4af04ba51c932514cb9740450d750aead5aaf8eb
Yehor <yehorsorokin@icloud.com>
2021-03-28 17:18:01 +0300

    Update template.php

--- sync/templates/template.php
+++ sync/templates/template.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>
-            TEST | YEHOR SOROKIN
+            Welcome
         </title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
